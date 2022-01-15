adcf2994b827f560f3a5f4e03843472a2aa5ddcd
greg <gregorybwest@gmail.com>
2022-01-14 18:35:12 -0800

    adds CRUD actions to find actor

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,7 +1,9 @@
 class ActorsController < ApplicationController
+  
   def print_actor
     actor_id = params[:id]
-    actor = Actor.find_by(id: actor_id)
+    actor = Actor.find(actor_id)
     render json: actor
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   get "/actor" => "actors#print_actor"
+  get "actor/:id" => "actors#print_actor"
+  post "/actor" => "actors#print_actor"
   get "/movies" => "movies#index"
   get "/movies/:id" => "movies#show"
+  
 end
 
 
