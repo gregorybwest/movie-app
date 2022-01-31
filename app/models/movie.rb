08808937b2e5ac7e8f26a746f945fde8eb9e0a80
greg <gregorybwest@gmail.com>
2022-01-30 22:19:18 -0800

    Genre and MovieGenre models and associations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -6,6 +6,8 @@ class Movie < ApplicationRecord
   validates :plot, presence: true, length: { in: 6..300 }
 
   has_many :actors
+  has_many :movie_genres
+  has_many :genres, through: :movie_genres
 
   
 end
